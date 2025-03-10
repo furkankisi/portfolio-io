92486940d428eb66b9b78f553684de4217ebc52d
Furkan Kişi <furkankisi0@gmail.com>
2025-03-10 08:21:06 +0300

    second

--- my-portfolio/src/App.jsx
+++ my-portfolio/src/App.jsx
@@ -1,12 +1,28 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
   const [isOpen, setIsOpen] = useState(false);
+  const [dateTime, setDateTime] = useState({
+    date: new Date().toLocaleDateString(),
+    time: new Date().toLocaleTimeString(),
+  });
 
   const toggleMenu = () => {
     setIsOpen(!isOpen);
   };
+
+  // Tarih ve saat bilgisini güncelleyen useEffect
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setDateTime({
+        date: new Date().toLocaleDateString(),
+        time: new Date().toLocaleTimeString(),
+      });
+    }, 1000);
+
+    return () => clearInterval(interval); // Bellek sızıntısını önler
+  }, []);
 
   return (
     <>
@@ -22,18 +38,24 @@ function App() {
           {isOpen && (
             <div className="start-menu">
               <div className="user">
-                <img src="/photo.jpg" alt=""  className='userPhoto'/>
+                <img src="/photo.jpg" alt="" className="userPhoto" />
                 <h3>Furkan Kişi</h3>
               </div>
-              <div className='menu-items'>
-                <div className='menu-item-first'>
+
+              {/* Tarih ve Saat Alanı */}
+              <div className="datetime">
+                <span className="time">{dateTime.time}</span>
+                <span className="date">{dateTime.date}</span>
+              </div>
+
+              <div className="menu-items">
+                <div className="menu-item-first">
                   <p>Internet</p>
                   <p>E-Mail</p>
                   <p>Notepad</p>
                   <p>Calculator</p>
-                
                 </div>
-                <div className='menu-item-second'>
+                <div className="menu-item-second">
                   <p>Projeler</p>
                   <p>Hakkımda</p>
                   <p>Ayarlar</p>
